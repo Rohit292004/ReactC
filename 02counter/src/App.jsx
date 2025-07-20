1fc7f4a78ed010d6f35f7eadd7d45458dd051d66
Rohit Kumar Singh <cenajohn5352@gmail.com>
2025-07-20 14:44:35 +0530

    added if condition

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,20 +5,25 @@ import './App.css'
 
 function App() {
 
-  let [counter, setCounter] = useState(15)
+  let [counter, setCounter] = useState(1)
 
   // let counter = 5
 
+  
   const addValue = () => {
     // counter = counter + 1
+    if(counter <= 19){
     setCounter(counter + 1)
+    }
       // console.log("clicked", counter)
 
   }
 
   const removeValue = () => {
+    if(counter >= 1){
     setCounter(counter - 1)
   }
+}
   return (
     <>
      <h1>Chai aur react</h1>
